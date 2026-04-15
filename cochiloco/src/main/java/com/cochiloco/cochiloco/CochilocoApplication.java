8cc55d6be9f67022b719a8215a725be76ec67239
Ghetto14 <inocencio2004jero@gmail.com>
2026-04-14 18:35:15 -0600

    Primer commit - Proyecto CochiLoco

---
+++ cochiloco/src/main/java/com/cochiloco/cochiloco/CochilocoApplication.java
@@ -0,0 +1,13 @@
+package com.cochiloco.cochiloco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CochilocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CochilocoApplication.class, args);
+	}
+
+}
---
+++ cochiloco/src/test/java/com/cochiloco/cochiloco/CochilocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cochiloco.cochiloco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CochilocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
